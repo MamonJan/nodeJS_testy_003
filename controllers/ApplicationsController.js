1c1a35cff01436342d27beaacef97a55aaf6e743
MamonJan <mamonjan33@gmail.com>
2019-10-03 13:52:03 +0200

    midlleware

--- controllers/ApplicationsController.js
+++ controllers/ApplicationsController.js
@@ -11,28 +11,6 @@ exports.store = (req, res, next) => {
         req.flash('form', req.body.imie + ', you are a true hero!');
         res.redirect('/');
     }).catch(next);
- // }).catch(function (){
- //     req.flash('form', 'Są problemy z danymi');
- //     res.redirect('/');
- // });
-
-
-// }, function (){
-//     //prosta obsługa po obietnicy
-//     req.flash('form', 'Są problemy z danymi');
-//     res.redirect('/');
-
-
-    // req.flash('form', req.body.name.split(' ')[0] + ', you are a true hero!');
-    // w zamian dodałem midlleware normalizeData, który modyfikuje nam to samo
-    // req.flash('form', req.body.imie + ', you are a true hero!');
-    // res.redirect('/');
-    // res.json({
-    //     'name': req.body.name,
-    //     'phone': req.body.phone,
-    //     'message': req.body.message
-    // })
-};
 
 // midlleware
 exports.normalizeData = (req, res, next) => {
