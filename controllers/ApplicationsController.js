31c5460c79ae8563133c069eac19780f13c824dc
MamonJan <mamonjan33@gmail.com>
2019-10-03 15:35:43 +0200

    midlleware end 2

--- controllers/ApplicationsController.js
+++ controllers/ApplicationsController.js
@@ -1,18 +1,17 @@
 const Application = require('../models/application');
 
 // Store new application
-exports.store = (req, res, next) => {
+exports.store = async (req, res, next) => {
 
     const application = Application.create({
         'name': req.body.imie,
         'phone': req.body.phone,
         'message': req.body.message
-    }).then().then(function(){
-        req.flash('form', req.body.imie + ', you are a true hero!');
-        res.redirect('/');
-    }).catch(next);
+    })
+    req.flash('form', req.body.imie + ', you are a true hero!');
+    res.redirect('/');
+};
 
-// midlleware
 exports.normalizeData = (req, res, next) => {
     const nameArr = req.body.name.split(' ');
 
